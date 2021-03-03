b47308cf9d1e928272b2c6a41f874cfaf6d47436
Kyungjoo Ha <429why@gmail.com>
2021-03-04 00:04:33 +0900

    answer added

--- baekjoon/data_structure/2164.py
+++ baekjoon/data_structure/2164.py
@@ -6,7 +6,7 @@ import collections
 n = int(input())
 cards_deque = collections.deque([i for i in range(1, n + 1)])
 
-while len(cards_deque) != 1:
+while len(cards_deque) != 1: # 마지막 남은 카드의 개수가 1이 아닐 때까지 반복
     cards_deque.popleft()
     cards_deque.rotate(-1)
 
