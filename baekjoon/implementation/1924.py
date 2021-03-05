2015f65981f6be0a83f551057464f0f58657415e
Kyungjoo Ha <429why@gmail.com>
2021-03-06 00:00:31 +0900

    answer added

---
+++ baekjoon/implementation/1924.py
@@ -0,0 +1,16 @@
+# 백준 온라인 저지 1924번: 2007년
+# www.acmicpc.net/problem/1924
+
+x, y = map(int, input().split())
+
+months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+day = 0
+
+for i in range(1, x):
+    day += months[i - 1]
+
+day += y
+
+print(days[day % 7])
