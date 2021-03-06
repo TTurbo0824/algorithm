9348190684ba9221d51eac281aaeaaa99f95b391
Kyungjoo Ha <429why@gmail.com>
2021-03-07 02:41:34 +0900

    answer added

---
+++ baekjoon/brute_force/1543.py
@@ -0,0 +1,16 @@
+# 백준 온라인 저지 1543번: 문서 검색
+# https://www.acmicpc.net/problem/1543
+
+d = input()
+w = input()
+
+index = 0
+result = 0
+
+while len(d) - index >= len(w):
+    if d[index: index + len(w)] == w:
+        result += 1
+        index += len(w)
+    else:
+        index += 1
+print(result)
