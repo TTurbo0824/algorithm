307abd97ea718a2b1dcae36391a4d692ab091ebc
Kyungjoo Ha <429why@gmail.com>
2021-03-07 20:23:14 +0900

    answer added

---
+++ baekjoon/sorting/1159.py
@@ -0,0 +1,26 @@
+# 백준 온라인 저지 1159번: 농구 경기
+# www.acmicpc.net/problem/1159
+
+import sys
+
+n = int(sys.stdin.readline())
+
+members = {}
+
+for _ in range(n):
+    member = sys.stdin.readline().strip()
+    if member[0] in members:
+        members[member[0]] += 1
+    else:
+        members[member[0]] = 1
+
+team = []
+for member, number in members.items():
+    if number >= 5:
+        team.append(member)
+
+team.sort()
+if team:
+    print(*team, sep='')
+else:
+    print("PREDAJA")
