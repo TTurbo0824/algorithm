2fb1710aeaf45b5b7221501d4cdd75912732f019
Kyungjoo Ha <429why@gmail.com>
2021-03-08 14:50:32 +0900

    answer added

---
+++ baekjoon/brute_force/1668.py
@@ -0,0 +1,21 @@
+# 백준 온라인 저지 1668번: 트로피 진열
+# https://www.acmicpc.net/problem/1668
+
+def ascending(array):
+    now = array[0]
+    count = 1
+    for i in range(1, len(array)):
+        if now < array[i]:
+            count += 1
+            now = array[i]
+    return count
+
+n = int(input())
+arr = []
+
+for _ in range(n):
+    arr.append(int(input()))
+
+print(ascending(arr))
+arr.reverse()
+print(ascending(arr))
--- baekjoon/data_structure/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-arr = []
-for _ in range(9):
-    arr.append(sys.stdin.readline().strip())
-
-
-max_num = max(arr)
-print(arr.index(max_num))
