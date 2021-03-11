b5f08005c95d086ae4ec374f7f429cb2f4adcf81
Kyungjoo Ha <429why@gmail.com>
2021-03-12 08:46:29 +0900

    answer added

---
+++ baekjoon/implementation/1157.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 1157번: 단어 공부
+# https://www.acmicpc.net/problem/1157
+
+a = input().upper()
+word = list(set(a))
+new = []
+
+for i in word:
+    count = a.count(i)
+    new.append(count)
+                    
+if new.count(max(new)) >= 2:
+    print("?")
+else:
+    print(word[new.index(max(new))])
