78fb7c7aa0923b17eef12f062a3ac2ed2da7186a
Kyungjoo Ha <429why@gmail.com>
2021-03-14 15:50:52 +0900

    answer added

---
+++ baekjoon/implementation/1236.py
@@ -0,0 +1,19 @@
+# 백준 온라인 저지 1236번: 성 지키기
+# https://www.acmicpc.net/problem/1236
+
+n, m = map(int, input().split())
+array = []
+
+for _ in range(n):
+    array.append(input())
+
+row_count, col_count = 0, 0
+
+for i in range(n):
+    if 'X' not in array[i] :
+        row_count += 1
+        
+for j in range(m):
+    if "X" not in [array[i][j] for i in range(n)]:        
+        col_count += 1
+print(max(row_count, col_count))
