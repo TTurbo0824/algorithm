fdbbd6e4b3995f675419b08d62d5440ce15cc635
Kyungjoo Ha <429why@gmail.com>
2021-03-24 09:16:08 +0900

    anser added

---
+++ baekjoon/data_structure/7785.py
@@ -0,0 +1,23 @@
+# 백준 온라인 저지 7785번: 회사에 있는 사람
+# https://www.acmicpc.net/problem/7785
+
+import sys
+input = sys.stdin.readline
+
+working = {}
+for _ in range(int(input())):
+    a, b = map(str, input().split())
+    if b == "enter":
+        working[a] = 1
+    elif b == "leave":
+        working[a] = 0
+        
+answer = []
+
+for people in working:
+    if working[people]:
+        answer.append(people)
+
+answer.sort(reverse=True)
+
+print(*answer, sep="\n")
