5915feac093c10715c28eb7fa56d90c00b238363
Kyungjoo Ha <429why@gmail.com>
2021-04-05 19:30:48 +0900

    answer added

---
+++ baekjoon/math/9094.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 9094번: 수학적 호기심
+# https://www.acmicpc.net/problem/9094
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    count = 0
+
+    for a in range(1, n - 1):
+        for b in range(a + 1, n):
+            if (a**2 + b**2 + m) % (a*b) == 0:
+                count += 1
+    print(count)
