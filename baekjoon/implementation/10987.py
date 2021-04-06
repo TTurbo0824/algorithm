e848a0da8a1d6933bbbfaa1fd39dfcbb735a84e3
Kyungjoo Ha <429why@gmail.com>
2021-04-06 19:23:42 +0900

    answer added

---
+++ baekjoon/implementation/10987.py
@@ -0,0 +1,12 @@
+# 백준 온라인 저지 10987번: 모음의 개수
+# https://www.acmicpc.net/problem/10987
+
+vowels = ["a", "e", "i", "o", "u"]
+word = list(map(str, input()))
+count = 0
+
+for letter in word:
+    if letter in vowels:
+        count += 1
+        
+print(count)
