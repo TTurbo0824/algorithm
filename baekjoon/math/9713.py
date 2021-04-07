99476f62ea3b2fe62e2c5f9305bf14d6b287cc94
Kyungjoo Ha <429why@gmail.com>
2021-04-07 15:29:22 +0900

    answer added

---
+++ baekjoon/math/9713.py
@@ -0,0 +1,9 @@
+# 백준 온라인 저지 9713번: Sum of Odd Sequence
+# https://www.acmicpc.net/problem/9713
+
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    print(((n//2)+1)**2)
