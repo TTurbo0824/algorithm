ff4fb5ccbb5923b328efa4afcc88ac48a73f60b3
Kyungjoo Ha <429why@gmail.com>
2021-05-24 11:13:49 +0900

    answer added

---
+++ baekjoon/backtracking/15650.py
@@ -0,0 +1,13 @@
+# 백준 온라인 저지 15650번: N과 M (2)
+# https://www.acmicpc.net/problem/15650
+
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+p = combinations(range(1, n + 1), m)
+
+for i in p:
+    print(' '.join(map(str, i)))
