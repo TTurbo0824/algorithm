e57485a5fc5bf4c70000b3ad9fb393dfd074f638
Kyungjoo Ha <429why@gmail.com>
2021-05-25 22:12:12 +0900

    answer added

--- baekjoon/backtracking/15649.py
+++ baekjoon/backtracking/15649.py
@@ -7,7 +7,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-p = permutations(range(1, n + 1), m)
+p = permutations(range(1, n), m)
 
 for i in p:
     print(' '.join(map(str, i)))
---
+++ baekjoon/backtracking/15651.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 15651번: N과 M (3)
+# https://www.acmicpc.net/problem/15651
+
+from itertools import product
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+p = product(range(1, n + 1), repeat = m)
+
+for i in p:
+    for j in i:
+        print(j, end = ' ')
+    print(end='\n')
