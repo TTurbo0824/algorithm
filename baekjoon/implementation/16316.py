81cd53ff80e6384755772b34b204e62048ee6976
Kyungjoo Ha <429why@gmail.com>
2021-05-31 06:53:27 +0900

    answer added

---
+++ baekjoon/implementation/16316.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 16316번: Baby Bites
+# https://www.acmicpc.net/problem/16316
+
+n = int(input())
+m = list(map(str, input().split()))
+answer = True
+
+for i in range(n):
+    if m[i] != str(i + 1) and m[i] != "mumble":
+        answer = False
+        print("something is fishy")
+        break
+    
+if answer == True:
+    print("makes sense")
