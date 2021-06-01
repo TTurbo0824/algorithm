127cba8ec2cca03c7027f454a29a9511d2771a74
Kyungjoo Ha <429why@gmail.com>
2021-06-02 08:39:35 +0900

    answer added

---
+++ baekjoon/implementation/4593.py
@@ -0,0 +1,25 @@
+# 백준 온라인 저지 4593번: Rock, Paper, Scissors
+# https://www.acmicpc.net/problem/4593
+
+while True:
+    n = list(map(str, input()))
+    m = list(map(str, input()))
+    
+    if n and m == ["E"]:
+        break
+    
+    game = len(n)
+    
+    p1 = 0
+    p2 = 0
+    
+    for i in range(game):
+        if n[i] == m[i]:
+            p1 += 0
+            p2 += 0
+        elif (n[i] == "R" and m[i] == "S") or (n[i] == "S" and m[i] == "P") or (n[i] == "P" and m[i] == "R"):
+            p1 += 1
+        else:
+            p2 += 1
+    
+    print("P1: ", p1, "\n", "P2: ", p2, sep="")
