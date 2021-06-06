93c840c13ab8750c587b6cfedc7c13214335ef96
Kyungjoo Ha <429why@gmail.com>
2021-06-06 15:12:45 +0900

    answer added

---
+++ baekjoon/implementation/4581.py
@@ -0,0 +1,22 @@
+# 백준 온라인 저지 4581번: Voting
+# https://www.acmicpc.net/problem/4581
+
+while True:
+    votes = input()
+    
+    if votes == "#":
+        break
+        
+    total = len(votes)
+    yes = votes.count("Y")
+    no = votes.count("N")
+    absent = votes.count("A")
+    
+    if absent >= total / 2:
+        print("need quorum")
+    elif yes > no:
+        print("yes")
+    elif no > yes:
+        print("no")
+    elif yes == no:
+        print("tie")
