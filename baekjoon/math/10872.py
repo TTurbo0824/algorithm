16c9210811fd613fbe2cf30f3ab92a8c7c00cb10
Kyungjoo Ha <429why@gmail.com>
2021-06-08 00:19:57 +0900

    new solution added

--- baekjoon/math/10872.py
+++ baekjoon/math/10872.py
@@ -1,11 +1,21 @@
 # 백준 온라인 저지 10872번: 팩토리얼
 # https://www.acmicpc.net/problem/10872
 
-def fact(n):
-    if n <= 1:
+# Soluion 1
+
+from math import factorial
+
+print(factorial(int(input())))
+
+# Solution 2
+
+'''
+def fact(num):
+    if num <= 1:
         return 1
     else:
-        return n * fact(n - 1)
+        return num * fact(num - 1)
 
-a = int(input())
-print(fact(a))
+n = int(input())
+print(fact(n))
+'''
