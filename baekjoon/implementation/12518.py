ace533b3cc35860e6e88b7501e8b984bcf9db35f
Kyungjoo Ha <429why@gmail.com>
2021-06-22 10:23:39 +0900

    answer added

---
+++ baekjoon/implementation/12518.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 12518번: Centauri Prime (Small2)
+# https://www.acmicpc.net/problem/12518
+
+import sys
+
+v = ['a', 'e', 'i', 'o', 'u']
+
+for _ in range(1, int(sys.stdin.readline()) + 1):
+    kingdom = sys.stdin.readline().strip()
+    if kingdom[-1].lower() == 'y':
+        print('Case #%d: %s is ruled by %s.'%(_, kingdom, 'nobody'))
+    elif kingdom[-1].lower() in v:
+        print('Case #%d: %s is ruled by %s.'%(_, kingdom, 'a queen'))
+    else:
+        print('Case #%d: %s is ruled by %s.'%(_, kingdom, 'a king'))
