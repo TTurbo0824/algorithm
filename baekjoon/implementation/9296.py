9793f9f611f98b2904018e6084465ef4dd465a33
Kyungjoo Ha <429why@gmail.com>
2021-06-26 20:22:33 +0900

    answer added

---
+++ baekjoon/implementation/9296.py
@@ -0,0 +1,16 @@
+# 백준 온라인 저지 9296번: Grading Exams
+# https://www.acmicpc.net/problem/9296
+
+import sys
+
+for _ in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+    answer = sys.stdin.readline().strip()
+    response = sys.stdin.readline().strip()
+    score = 0
+
+    for i in range(n):
+        if answer[i] != response[i]:
+            score += 1
+
+    print('Case %d: %d' % (_ + 1, score))
