b15b8ddd1a57fe4ce7926e65d743ea5d2048b7c8
Kyungjoo Ha <429why@gmail.com>
2021-06-27 17:29:18 +0900

    answer added

---
+++ baekjoon/implementation/13484.py
@@ -0,0 +1,26 @@
+# 백준 온라인 저지 13484번: Tarifa
+# https://www.acmicpc.net/problem/13484
+
+import sys
+
+input = sys.stdin.readline
+
+x = int(input())
+n = int(input())
+
+# Solution 1
+
+total = x * (n + 1)
+
+for _ in range(n):
+    p = int(input())
+    total -= p
+
+print(total)
+
+# Solution 2
+
+'''
+p = [int(input()) for _ in range(n)]
+print(x * (n + 1) - sum(p))
+'''
