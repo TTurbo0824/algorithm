4270d13db28a903ef645021607440581888aab62
Kyungjoo Ha <429why@gmail.com>
2021-06-29 00:03:20 +0900

    answer added

---
+++ baekjoon/math/13229.py
@@ -0,0 +1,13 @@
+# 백준 온라인 저지 13229번: Selection Sum
+# https://www.acmicpc.net/problem/13229
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    print(sum(arr[a:b+1]))
