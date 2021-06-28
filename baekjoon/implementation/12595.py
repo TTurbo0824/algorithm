8eae2f6195b0805695fd2eae782116cc7f03ce61
Kyungjoo Ha <429why@gmail.com>
2021-06-28 16:05:19 +0900

    answer added

---
+++ baekjoon/implementation/12595.py
@@ -0,0 +1,16 @@
+# 백준 온라인 저지 12595번: Odd Man Out (Small)
+# https://www.acmicpc.net/problem/12595
+
+import sys
+
+input = sys.stdin.readline
+
+count = 0
+
+for _ in range(int(input())):
+    n = int(input())
+    guests = list(map(int, input().split()))
+    for person in guests:
+        if guests.count(person) == 1:
+            count += 1
+            print('Case #%d: %d' %(count, person))
