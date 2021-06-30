6a5cb3c2dbea7e6db7918ca2236f95975c7ebad6
Kyungjoo Ha <429why@gmail.com>
2021-06-30 14:49:00 +0900

    answer added

---
+++ baekjoon/math/13297.py
@@ -0,0 +1,9 @@
+# 백준 온라인 저지 13297번: Quick Estimates
+# https://www.acmicpc.net/problem/13297
+
+import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    print(len(input().strip()))
