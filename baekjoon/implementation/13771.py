1e52150df04448e09dfe3661d743ddbb092878bf
Kyungjoo Ha <429why@gmail.com>
2021-07-01 09:05:43 +0900

    answer added

---
+++ baekjoon/implementation/13771.py
@@ -0,0 +1,14 @@
+# 백준 온라인 저지 13771번: Presents
+# https://www.acmicpc.net/problem/13771
+
+import sys
+
+input = sys.stdin.readline
+
+presents = []
+
+for _ in range(int(input())):    
+    presents.append(float(input()))
+
+presents.sort()
+print("%.2f"%presents[1])
