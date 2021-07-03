1e1ccd0064b2ef7dd928d7abe66895c2a0e6adce
Kyungjoo Ha <429why@gmail.com>
2021-07-04 00:13:14 +0900

    answer added

---
+++ baekjoon/implementation/9286.py
@@ -0,0 +1,34 @@
+# 백준 온라인 저지 9286번: Gradabase
+# https://www.acmicpc.net/problem/9286
+
+import sys
+
+input = sys.stdin.readline
+
+# Solution 1
+
+'''
+answer = []
+
+for t in range(1, int(input()) + 1):
+    answer.append(f'Case {t}:')
+    for _ in range(int(input())):
+        grade = int(input())
+        if grade < 6:
+            answer.append(grade + 1)
+
+print(*answer, sep='\n')
+'''
+
+# Solution 2
+
+answer = ''
+
+for t in range(1, int(input()) + 1):
+    answer += f'Case {t}:\n'
+    for _ in range(int(input())):
+        grade = int(input())
+        if grade < 6:
+            answer += f'{grade + 1}\n'
+
+print(answer)
