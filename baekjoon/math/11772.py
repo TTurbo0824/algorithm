eda2c1d0e8cb391bf0198df1ba6010a386b37a1b
Kyungjoo Ha <429why@gmail.com>
2021-07-16 03:20:29 +0900

    answer added

---
+++ baekjoon/math/11772.py
@@ -0,0 +1,14 @@
+# 백준 온라인 저지 11772번: POT
+# https://www.acmicpc.net/problem/11772
+
+import sys
+input = sys.stdin.readline
+
+ans = 0
+
+for _ in range(int(input())):
+    n = input().strip()
+    base = int(n[0:-1])
+    exponent = int(n[-1])
+    ans += base**exponent
+print(ans)
