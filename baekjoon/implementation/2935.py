fd02a628c3875a9cb38f0bc8bcb193fe1924623c
Kyungjoo Ha <429why@gmail.com>
2021-07-23 06:57:28 +0900

    answer added

---
+++ baekjoon/implementation/2935.py
@@ -0,0 +1,17 @@
+# 백준 온라인 저지 2935번: 소음
+# https://www.acmicpc.net/problem/2935
+
+# Solution 1
+
+s = ''
+for _ in range(3):
+    s += input()
+
+print(eval(s))
+
+
+# Solution 2
+
+'''
+print(eval(input()+input()+input()))
+'''
