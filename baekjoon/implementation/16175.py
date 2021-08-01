b2b9ada231ea3aec612c5bf3661a0afcf1d5651b
Kyungjoo Ha <429why@gmail.com>
2021-08-01 21:17:19 +0900

    answer added

---
+++ baekjoon/implementation/16175.py
@@ -0,0 +1,22 @@
+# 백준 온라인 저지 16175번: General Election
+# https://www.acmicpc.net/problem/16175
+
+import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    
+    total = []
+    candidates = [0] * n
+
+    for _ in range(m):
+        votes = list(map(int, input().split()))
+        total.append(votes)
+
+    for i in range(m):
+        for j in range(n):
+            candidates[j] += total[i][j]
+    
+    print(candidates.index(max(candidates)) + 1)
