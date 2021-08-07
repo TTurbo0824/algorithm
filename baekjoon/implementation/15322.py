a6f9420f7b4d7aee2435f8b6242e741d57a94f11
Kyungjoo Ha <429why@gmail.com>
2021-08-07 13:15:56 +0900

    answer added

---
+++ baekjoon/implementation/15322.py
@@ -0,0 +1,17 @@
+# 백준 온라인 저지 15322번: Košnja
+# https://www.acmicpc.net/problem/15322
+
+import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    nums = list(map(int, input().split()))
+    nums.sort()
+    print(2 * (nums[0] - 1))
+
+'''
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    print((min(n, m)- 1) * 2)
+'''
