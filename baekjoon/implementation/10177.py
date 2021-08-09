2630db479475e36514acb63cf28f4e47605af0cf
Kyungjoo Ha <429why@gmail.com>
2021-08-10 08:30:18 +0900

    write code more efficiently

--- baekjoon/implementation/10177.py
+++ baekjoon/implementation/10177.py
@@ -8,34 +8,35 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)] 
-    target = sum(arr[0])
-    isMagic = True
-    k = 0
-    m = 0
-    l = 0
+    target = sum(arr[0]) # 타겟 설정
+    col = 0 # 세로 비교 변수
+    leftDiagonal = 0 # 좌측 대각선 비교 변수
+    rightDiagonal = 0 # 우측 대각선 비교 변수
+    isMagic = True # Magic square 여부 판단
+
     for i in range(n):
+        # 가로줄 검사
         if target != sum(arr[i]):
             isMagic = False
             break   
-        m += arr[i][i]
-        l += arr[i][-(i+1)]
+        leftDiagonal += arr[i][i]
+        rightDiagonal += arr[i][-(i+1)]
+        
         if i == n - 1:
-            print(m, l)
-            if m != target:
+            # 좌측 대각선 검사
+            if leftDiagonal != target:
                 isMagic = False
                 break
-            elif l != target:
+            # 우측 대각선 검사
+            elif rightDiagonal != target:
                 isMagic = False
                 break
+        # 세로줄 검사
         for j in range(n):
-            k += arr[j][i]
+            col += arr[j][i]
             if j == n - 1:
-                if k != target:
+                if col != target:
                     isMagic = False
                     break
-                k = 0
-    
-    if isMagic == True:
-        print('Magic square of size %d'%n)
-    else:
-        print('Not a magic square')
+                col = 0
+    print("Magic square of size %d"%n if isMagic == True else "Not a magic square")
