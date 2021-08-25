16b3b98a1039490ba2a7aadee04e36950d439f24
Kyungjoo Ha <429why@gmail.com>
2021-08-25 22:37:32 +0900

    answer added

---
+++ baekjoon/brute_force/4375.py
@@ -0,0 +1,15 @@
+# 백준 온라인 저지 4375번: 1
+# https://www.acmicpc.net/problem/4375
+
+while True:
+    try:
+        n = int(input())
+        ans = '1'
+        while True:
+            if int(ans) % n == 0:
+                print(len(ans))
+                break
+            ans += '1'
+
+    except EOFError:
+        break
