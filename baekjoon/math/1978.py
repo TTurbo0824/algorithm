efd85db0aeeedeaebb62f11f5f1b1e1be847da5b
Kyungjoo Ha <429why@gmail.com>
2021-08-29 11:24:01 +0900

    answer added

---
+++ baekjoon/math/1978.py
@@ -0,0 +1,24 @@
+# 백준 온라인 저지 1978번: 소수 찾기
+# https://www.acmicpc.net/problem/1978
+
+a = int(input())
+
+arr = list(map(int, input().split()))
+
+def prime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    for i in range(2, num):
+        if num%i==0:
+            return False
+    return True
+
+count = 0
+
+for i in range(a):
+    if prime(arr[i]) == True:
+        count += 1
+
+print(count)
