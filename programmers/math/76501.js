31c388cbd738e0335950b4df1b73a860916244bb
Kyungjoo Ha <429why@gmail.com>
2021-09-19 10:28:20 +0900

    answer added

---
+++ programmers/math/76501.js
@@ -0,0 +1,14 @@
+// 프로그래머스 월간 코드 챌린지 시즌2 음양 더하기
+// https://programmers.co.kr/learn/courses/30/lessons/76501
+
+const Solution = (absolutes, signs) => {
+  let answer = 0;
+  
+  absolutes.map((el, idx) => {
+    signs[idx] === true
+    ? answer += el
+    : answer -= el
+  })
+  
+  return answer  
+}
