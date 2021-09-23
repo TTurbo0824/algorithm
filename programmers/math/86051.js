c306561505678dcc2661263c2cca7dd484504657
Kyungjoo Ha <429why@gmail.com>
2021-09-23 10:02:49 +0900

    answer edited

--- programmers/math/86051.js
+++ programmers/math/86051.js
@@ -1,7 +1,7 @@
 // 프로그래머스 월간 코드 챌린지 시즌3 없는 숫자 더하기
 // https://programmers.co.kr/learn/courses/30/lessons/86051
 
-const solution= (numbers) => {
+const solution = (numbers) => {
   let answer = 0;
   const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -10,4 +10,4 @@ const solution= (numbers) => {
   });
 
   return answer;
-}
+};
