a5315e15dca575e45502fbb1778c0355dc20c1ad
Kyungjoo Ha <429why@gmail.com>
2021-10-20 17:02:41 +0900

    modify: Source code simplified

--- programmers/brute force/42840.js
+++ programmers/brute force/42840.js
@@ -6,16 +6,11 @@ const answers = [1, 3, 2, 4, 2];
 const solution = (answers) => {
   let answer = [];
   // 세 사람 모두 각각의 다른 패턴을 가지고 있음
-  // 1번 다섯 숫자가 반복
-  // 2번 여덟 숫자가 반복
-  // 3번 열 개의 숫자가 반복
-  // 주어진 answers라는 배열의 길이를 포함 수 있는 새로운 배열을 생성해서 진행
-  // 패턴의 길이가 제각각이기 때문에 통일할 수 있는 방법이 없어 보임
-
   let first = [1, 2, 3, 4, 5];
   let second = [2, 1, 2, 3, 2, 4, 2, 5];
   let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
 
+  // 이 패턴이 answers 배열을 담을 수 있도록 확장 시킴
   first = Array(Math.ceil(answers.length / first.length))
     .fill(first)
     .flat();
@@ -28,27 +23,18 @@ const solution = (answers) => {
 
   let total = [0, 0, 0];
 
+  // answer 배열을 순회하며 각각의 수포자들이 몇 문제나 맞추었는지 계산
   answers.map((el, idx) => {
     if (first[idx] === el) total[0] += 1;
     if (second[idx] === el) total[1] += 1;
     if (third[idx] === el) total[2] += 1;
   });
 
-  // 최대값을 구해야함 => Math.max(...total)
-
+  // 최고점이 몇 점인지 도출 => Math.max(...total)
+  // 이 최고점을 받은 사람이 몇 번 째 수포자인지 판별 후 answer 배열에 push 해줌
   total.map((el, idx) => {
     if (el === Math.max(...total)) answer.push(idx + 1);
   });
 
   return answer;
 };
-
-console.log(solution(answers));
-
-// let first = [1, 2, 3, 4, 5];
-
-let first = [1, 2, 3, 4, 5];
-let second = [2, 1, 2, 3, 2, 4, 2, 5];
-let third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-// console.log(Array(Math.ceil(answers.length/first.length)).fill(first).flat() )
